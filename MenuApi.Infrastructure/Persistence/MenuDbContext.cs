11f050acd0a8ed34fb6be8413599ebf9b3768661
João Pedro gomes nascimento <joaopedrogomes26@hotmail.com>
2023-11-02 19:32:50 -0300

    Add context

---
+++ MenuApi.Infrastructure/Persistence/MenuDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MenuApi.Infrastructure.Persistence
+{
+    public class MenuDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => 
+            optionsBuilder.UseSqlite("DataSource=menuDb.db;Cache=Shared");
+    }
+}
