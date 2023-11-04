a3771a4ecab5773c2b78d74e094c286708da9b72
João Pedro gomes nascimento <joaopedrogomes26@hotmail.com>
2023-11-03 23:10:26 -0300

    Add menu controller

---
+++ MenuApi/Controllers/MenuController.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using MenuApi.Application.Commands.CreateMenu;
+using MenuApi.Application.Queries.GetMenuById;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MenuApi.Controllers
+{
+    [Route(template: "api/menus")]
+    public class MenuController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public MenuController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CreateMenuCommand command)
+        {
+            var id = await _mediator.Send(command);
+            return CreatedAtAction(nameof(GetById), new { id }, command);
+        }
+        [HttpGet(template: "{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var query = new GetMenuByIdQuery(id);
+            var menu = await _mediator.Send(query);
+
+            if (menu == null)
+                return NotFound();
+
+            return Ok(menu);
+
+        }
+    }
+}
